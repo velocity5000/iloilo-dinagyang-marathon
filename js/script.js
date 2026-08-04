8de16b82095b39a115dcd3aa763fae08f48aaf7b
Jan Mark Caratao <velocity5000.ph@gmail.com>
2026-08-04 23:27:40 +0800

    Updated css for laptop view

--- js/script.js
+++ js/script.js
@@ -199,3 +199,21 @@ if (floatingBar) {
     });
 
 }
+
+// ==========================
+// MOBILE MENU
+// ==========================
+
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+if (hamburger && navLinks) {
+
+    hamburger.addEventListener("click", () => {
+
+        hamburger.classList.toggle("active");
+        navLinks.classList.toggle("active");
+
+    });
+
+}
